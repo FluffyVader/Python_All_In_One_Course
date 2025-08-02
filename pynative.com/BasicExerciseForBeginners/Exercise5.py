c2290b8bc03add51dc71465bf5fed407512f2779
FluffyVader <wewilllearnthis@gmail.com>
2025-08-02 22:24:02 +0100

    Fixed an empty list case in the exercise #5.
    Covered the empty list case because if you give an empty list as an argument for out function an error in console/terminal appears.

--- pynative.com/BasicExerciseForBeginners/Exercise5.py
+++ pynative.com/BasicExerciseForBeginners/Exercise5.py
@@ -15,7 +15,12 @@ def first_and_last_chars_same_or_not(list_of_numbers):
     first_index_value = 0
     last_index_value = len(list_of_numbers) - 1
 
-    if list_of_numbers[last_index_value] == list_of_numbers[first_index_value]:
+
+    if len(list_of_numbers) == 0:
+        print("Sorry, you can not use an empty list as an argument for the function")
+        return False
+
+    elif list_of_numbers[last_index_value] == list_of_numbers[first_index_value]:
         print("The first and last values of the list are the same")
         return True
 
@@ -24,3 +29,6 @@ def first_and_last_chars_same_or_not(list_of_numbers):
         return False
 
 first_and_last_chars_same_or_not([10, 20, 20, 20, 20, 30, 10, 20])
+first_and_last_chars_same_or_not([10, 20, 20, 20, 20, 30, 10, 10])
+first_and_last_chars_same_or_not([45])
+first_and_last_chars_same_or_not([])
