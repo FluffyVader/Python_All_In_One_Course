ab8f30cbef775d72521f26722fc4d91054b0f2e8
FluffyVader <wewilllearnthis@gmail.com>
2025-08-08 23:36:51 +0100

    Word Capital
    Makes every first letter of a word capital.

--- pynative.com/BasicExerciseForBeginners/Exericise22.py
+++ pynative.com/BasicExerciseForBeginners/Exericise22.py
@@ -3,10 +3,29 @@
 
 # str1 = "pynative.com is for python lovers"
 # Output Pynative.com Is For Python Lovers
-
+####################################################################
 str_to_capitalize = "pynative.com is for python lovers"
 
 for first_symbol in str_to_capitalize:
     str_to_capitalize = str_to_capitalize.title()
 
 print(str_to_capitalize)
+####################################################################
+#split str, capitalize every word (makes the first letter capital in each word), then print it
+
+str_to_capitalize_tokens = str_to_capitalize.split()
+capitalized_str = ""
+
+for index, item in enumerate(str_to_capitalize_tokens):
+    capitalized_item = item.capitalize()
+
+    capitalized_str += capitalized_item
+    if index != (len(str_to_capitalize_tokens) - 1):
+        capitalized_str += " "
+
+#capitalized_str = capitalized_str.strip()
+
+print("-_-_-_-_-_-_")
+print(capitalized_str)
+#print(len(str_to_capitalize))
+#print(len(capitalized_str))
---
+++ test1.py
@@ -0,0 +1,18 @@
+# import time
+# import random
+
+# start = 0
+# end = 10000
+# step = 1
+
+# list_1 = []
+# for i in range(start, end, step):
+#     random_number = random.randint(0, 10000)
+#     list_1.append(random_number)
+
+# start_time = time.time()
+# list_1.sort()
+# print(f"the first number of list_1 is {list_1[start], list_1[end-1]}")
+# end_time = time.time()
+# print(f"the time to sort the list is: {end_time - start_time}")
+
