6b9f51ff36146c3ba3b077330dfea918c4aaba76
FluffyVader <wewilllearnthis@gmail.com>
2025-08-13 10:37:50 +0100

    Counts how much a word has appeared in a list
    Counts how much a word has appeared in a list

---
+++ pynative.com/BasicExerciseForBeginners/ListExercise/Exercise7.py
@@ -0,0 +1,12 @@
+# Exercise 7: Count Occurrences
+# Count and print how many times 'Football' appears in list.
+
+# Given:
+
+# sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis'].
+
+sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis']
+
+counter = sports.count("Football")
+
+print(f"The word Football has appeared {counter} times")
