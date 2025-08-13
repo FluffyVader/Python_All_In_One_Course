25792108ea9b0b9a58ad60f1ecca2b597883e5f1
FluffyVader <wewilllearnthis@gmail.com>
2025-08-13 16:53:31 +0100

    Update
    There is now a custom count function

--- pynative.com/BasicExerciseForBeginners/ListExercise/Exercise7.py
+++ pynative.com/BasicExerciseForBeginners/ListExercise/Exercise7.py
@@ -5,8 +5,19 @@
 
 # sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis'].
 
+############################################################################
+def str_counter(given_list, word_to_find):
+    word_counter = 0
+    for word in given_list:
+        if word == word_to_find:
+            word_counter += 1
+    
+    return word_counter
+
+############################################################################
+
 sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis']
+counter2 = sports.count("Football")
+print(f"The word Football has appeared {counter2} times")
 
-counter = sports.count("Football")
-
-print(f"The word Football has appeared {counter} times")
+print(f"The word Football has appeared {str_counter(sports, "Football")} times")
