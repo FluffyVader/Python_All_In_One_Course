9bde485f699b73d9d2b2794deef84d7481a8b0cd
FluffyVader <wewilllearnthis@gmail.com>
2025-08-14 18:29:19 +0100

    Sorting
    2 method's of sorting a list

---
+++ pynative.com/BasicExerciseForBeginners/ListExercise/Exercise8.py
@@ -0,0 +1,21 @@
+# Exercise 8: Sort a list of numbers
+# Sort a given list of numbers in ascending order and print it.
+
+# Given: numbers = [5, 2, 8, 1, 9]
+
+# Expected Output:
+
+# Original list: [5, 2, 8, 1, 9]
+# Sorted list: [1, 2, 5, 8, 9]
+
+list_to_sort = [5, 2, 8, 1, 9]
+
+list_to_sort.sort()
+
+print(list_to_sort)
+#####################################c
+list_to_sort2 = [5, 2, 8, 1, 9]
+
+list_to_sort2 = sorted(list_to_sort2)
+
+print(list_to_sort2)
